9d751b72d5d9793396505f8d32000f22ae671737
Nazmos27 <nazmos789@gmail.com>
2024-09-11 02:22:28 +0600

    utility function to reduce redundency made for admin role

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,20 +1,13 @@
-import { Layout, Menu} from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { createElement } from 'react';
+import { Layout, Menu } from "antd";
 
-
+import {  Outlet } from "react-router-dom";
+import { adminSidebarItems } from "../../routes/admin.routes";
 
 const { Header, Content, Footer, Sider } = Layout;
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-    (icon, index) => ({
-      key: String(index + 1),
-      icon: createElement(icon),
-      label: `nav ${index + 1}`,
-    }),
-  );
+
 const MainLayout = () => {
   return (
-    <Layout style={ {height : '100vh'}}>
+    <Layout style={{ height: "100vh" }}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
@@ -25,27 +18,42 @@ const MainLayout = () => {
           console.log(collapsed, type);
         }}
       >
-        <div style={{color : 'white', height : '4rem', display : 'flex', justifyContent : 'center', alignItems : 'center'}}><h1>PH Uni</h1></div>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+        <div
+          style={{
+            color: "white",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>PH Uni</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={adminSidebarItems}
+        />
       </Sider>
       <Layout>
         <Header style={{ padding: 0 }} />
-        <Content style={{ margin: '24px 16px 0' }}>
+        <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
               padding: 24,
-              minHeight: 360
+              minHeight: 360,
             }}
           >
-            content
+            <Outlet></Outlet>
           </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
+        <Footer style={{ textAlign: "center" }}>
           Ant Design ©{new Date().getFullYear()} Created by Ant UED
         </Footer>
       </Layout>
     </Layout>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;

e7cce715ea775196d4ef4ab2effebb4bbe936252
Nazmos27 <nazmos789@gmail.com>
2024-09-12 02:13:19 +0600

    login form and server integration successful

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,37 @@
+import { Button } from "antd"
+import { useForm } from "react-hook-form"
+import { useLoginMutation } from "../redux/features/auth/authApi"
+
+
 
 const Login = () => {
+
+  const {register, handleSubmit} = useForm()
+
+  const [ login, {data, error}] = useLoginMutation()
+  console.log("data =>", data);
+  console.log("error =>", error);
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id : data.id,
+      password : data.password
+    }
+    login(userInfo)
+  }
+
   return (
-    <div>
-        <h1>Login Page</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID : </label>
+        <input type="text" id="id" {...register('id')}></input>
+      </div>
+      <div>
+        <label htmlFor="password">Password : </label>
+        <input type="text" id="password" {...register('password')}></input>
+      </div>
+      <Button htmlType="submit">Log in</Button>
+    </form>
   )
 }
 

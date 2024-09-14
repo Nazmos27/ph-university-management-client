52233a1d4edbad0cd3c5db5b4ce4669424bfac86
Nazmos27 <nazmos789@gmail.com>
2024-09-14 12:07:21 +0600

    accessToken retrieved through refreshToken fx done

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -4,75 +4,71 @@ import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 import { NavLink } from "react-router-dom";
-import AcademicManagement from "../pages/admin/AcademicManagement";
-
-
+import AcademicSemester from "../pages/admin/academicManagement/AcademicSemester";
 
 type TAdminSidebarItem = {
-    key : string,
-    label : ReactNode,
-    children ?: TAdminSidebarItem[]
-}
+  key: string;
+  label: ReactNode;
+  children?: TAdminSidebarItem[];
+};
 
 export const adminPaths = [
-    {
-        name : 'Dashboard',
-        path : 'dashboard',
-        element : <AdminDashboard/>
-    },
-    {
-        name : 'Academic Management',
-        path : 'academic-semesters',
-        children : [
-            {
-                name : 'Academic Semester',
-                path : 'academic-semesters',
-                element : <AcademicManagement/>
-            }
-        ]
-    },
-    {
-        name : 'User Management',
-        children : [
-            {
-                name : 'Create Admin',
-                path : 'create-admin',
-                element : <CreateAdmin/>
-            },
-            {
-                name : 'Create Faculty',
-                path : 'create-faculty',
-                element : <CreateFaculty/>
-            },
-            {
-                name : 'Create Student',
-                path : 'create-student',
-                element : <CreateStudent/>
-            },
-            
-        ]
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    name: "Academic Management",
+    children: [
+      {
+        name: "Academic Semester",
+        path: "academic-semesters",
+        element: <AcademicSemester />,
+      },
+    ],
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+    ],
+  },
+];
+
+export const adminSidebarItems = adminPaths.reduce(
+  (acc: TAdminSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
     }
-]
-
-
-
-
-export const adminSidebarItems = adminPaths.reduce((acc : TAdminSidebarItem[] , item) => {
-    if(item.path && item.name){
-        acc.push({
-            key : item.name,
-            label : <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
-        })
-    }
-    if(item.children){
-        acc.push({
-            key : item.name,
-            label : item.name,
-            children : item.children.map((child) => ({
-                key : child.name,
-                label : <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
-            }))
-        })
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
     }
     return acc;
-},[])
+  },
+  []
+);
